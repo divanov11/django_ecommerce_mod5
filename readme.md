36e83b27fe7d0d57a421058bdba960a46341d268
divanov11 <ivanovsin11@gmail.com>
2024-11-26 11:35:59 -0800

    Added readme

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Django eCommerce website
+
+Basic django eCommerce website with checkout guest checkout and paypal integration
+
+Installation:
+
+1. Clone repo: `git clone <repo_url>`
+2. Install dependencies: `pip install -r requirements.txt`
+3. Run server: `python manage.py runserver`
+
+<img src="./static/images/demo.png"/>
--- static/js/cart.js
+++ static/js/cart.js
@@ -1,63 +1,62 @@
-var updateBtns = document.getElementsByClassName('update-cart')
+var updateBtns = document.getElementsByClassName("update-cart");
 
 for (i = 0; i < updateBtns.length; i++) {
-	updateBtns[i].addEventListener('click', function(){
-		var productId = this.dataset.product
-		var action = this.dataset.action
-		console.log('productId:', productId, 'Action:', action)
-		console.log('USER:', user)
+    updateBtns[i].addEventListener("click", function () {
+        var productId = this.dataset.product;
+        var action = this.dataset.action;
+        console.log("productId:", productId, "Action:", action);
+        console.log("USER:", user);
 
-		if (user == 'AnonymousUser'){
-			addCookieItem(productId, action)
-		}else{
-			updateUserOrder(productId, action)
-		}
-	})
+        if (user == "AnonymousUser") {
+            addCookieItem(productId, action);
+        } else {
+            updateUserOrder(productId, action);
+        }
+    });
 }
 
-function updateUserOrder(productId, action){
-	console.log('User is authenticated, sending data...')
+function updateUserOrder(productId, action) {
+    console.log("User is authenticated, sending data...");
 
-		var url = '/update_item/'
+    var url = "/update_item/";
 
-		fetch(url, {
-			method:'POST',
-			headers:{
-				'Content-Type':'application/json',
-				'X-CSRFToken':csrftoken,
-			}, 
-			body:JSON.stringify({'productId':productId, 'action':action})
-		})
-		.then((response) => {
-		   return response.json();
-		})
-		.then((data) => {
-		    location.reload()
-		});
+    fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "X-CSRFToken": csrftoken,
+        },
+        body: JSON.stringify({ productId: productId, action: action }),
+    })
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            location.reload();
+        });
 }
 
-function addCookieItem(productId, action){
-	console.log('User is not authenticated')
+function addCookieItem(productId, action) {
+    console.log("User is not authenticated");
 
-	if (action == 'add'){
-		if (cart[productId] == undefined){
-		cart[productId] = {'quantity':1}
+    if (action == "add") {
+        if (cart[productId] == undefined) {
+            cart[productId] = { quantity: 1 };
+        } else {
+            cart[productId]["quantity"] += 1;
+        }
+    }
 
-		}else{
-			cart[productId]['quantity'] += 1
-		}
-	}
+    if (action == "remove") {
+        cart[productId]["quantity"] -= 1;
 
-	if (action == 'remove'){
-		cart[productId]['quantity'] -= 1
+        if (cart[productId]["quantity"] <= 0) {
+            console.log("Item should be deleted");
+            delete cart[productId];
+        }
+    }
+    console.log("CART:", cart);
+    document.cookie = "cart=" + JSON.stringify(cart) + ";domain=;path=/";
 
-		if (cart[productId]['quantity'] <= 0){
-			console.log('Item should be deleted')
-			delete cart[productId];
-		}
-	}
-	console.log('CART:', cart)
-	document.cookie ='cart=' + JSON.stringify(cart) + ";domain=;path=/"
-	
-	location.reload()
+    location.reload();
 }
